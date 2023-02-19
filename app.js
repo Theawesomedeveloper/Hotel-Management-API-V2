0bc3cf2c7bc3a353c6f37fcf1ae11a508f56b8d2
Jonathan <113763057+Theawesomedeveloper@users.noreply.github.com>
2023-02-19 15:49:30 +0100

    Added cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const { connect } = require('mongoose');
 const { PORT } = require('./config')
-const rootRoute = require('./routes/index.route')
+const rootRoute = require('./routes/index.route');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 app.use('/api/v2', rootRoute)
 
 
